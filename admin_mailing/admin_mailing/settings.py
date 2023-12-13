5fa3258bcb8a48320c74282e69bc7e66695efa63
veneberg <grebenev-81@mail.ru>
2023-12-13 21:07:48 +0300

    .env edit db settings

--- admin_mailing/admin_mailing/settings.py
+++ admin_mailing/admin_mailing/settings.py
@@ -77,14 +77,15 @@ WSGI_APPLICATION = 'admin_mailing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'admin_mailing_service_db',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': os.getenv("DB_ENGINE"),
+        'NAME': os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
