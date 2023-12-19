3f22250f5e3b55d3730850262a93411d9b012ace
veneberg <grebenev-81@mail.ru>
2023-12-19 13:18:35 +0300

    frontend add ClientDispatches.js

--- admin_mailing/frontend/src/components/ClientDispatches.js
+++ admin_mailing/frontend/src/components/ClientDispatches.js
@@ -19,22 +19,24 @@ const ClientDispatches = ({clients, dispatches}) => {
     const filtered_client = clients.find((client) => client.uu_id === params.uu_id);
     const filtered_client_phone_number = filtered_client?.phone_number;
     const filtered_dispatches = dispatches.filter((dispatch) => {
-          if (dispatch.tag_filter && dispatch.operator_code_filter) {
+        if (dispatch.tag_filter && dispatch.operator_code_filter) {
             return dispatch.tag_filter === filtered_client?.tag && dispatch.operator_code_filter === filtered_client?.operator_code;
-          } else if (dispatch.tag_filter) {
+        } else if (dispatch.tag_filter) {
             return dispatch.tag_filter === filtered_client?.tag;
-          } else if (dispatch.operator_code_filter) {
+        } else if (dispatch.operator_code_filter) {
             return dispatch.operator_code_filter === filtered_client?.operator_code;
-          }
-          return false;
-        });
+        }
+        return false;
+    });
     return (
         <div className="section is-medium">
             <p className="title">
-                Рассылки клиента с номером телефона {filtered_client_phone_number}
+                Рассылки клиента с номером
+                телефона {filtered_client_phone_number}
             </p>
             <p className="subtitle">
-                таблица рассылок клиента с номером {filtered_client_phone_number}
+                таблица рассылок клиента с
+                номером {filtered_client_phone_number}
             </p>
             <table className="table is-narrow is-fullwidth">
                 <thead className="has-background-info-light">
@@ -45,9 +47,11 @@ const ClientDispatches = ({clients, dispatches}) => {
                 <th>Тег</th>
                 <th>Код оператора</th>
                 </thead>
-                {filtered_dispatches.map((dispatch) => <DispatchItem dispatch={dispatch}/>)}
+                {filtered_dispatches.map((dispatch) => <DispatchItem
+                    dispatch={dispatch}/>)}
             </table>
-            <Link class='button is-light' to='/dispatches/create'>Создать рассылку</Link>
+            <Link class='button is-light' to='/dispatches/create'>Создать
+                рассылку</Link>
         </div>
     )
 }
